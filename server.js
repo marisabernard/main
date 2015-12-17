f0d9a6ad12639d1ebff2e6af00dfa2a8305bf224
marisabernard <mlbernard91@gmail.com>
2015-12-16 22:59:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-var express = require('express');
+/*eslint-env node */
+var express   	 	= require('express'); 
+var bodyParser	 	= require('body-parser');
+var path 		 	= require('path');
+var logger 		 	= require('morgan');
+var methodOverride 	= require('method-override');
+var session 		= require('express-session');
+var multer 			= require('multer');
+var ejs 			= require('ejs');
+var favicon         = require('serve-favicon');
+
+
+// APP CONFIGURATION ==================================================================
 var app = express();
+app.set('views', path.join(__dirname, 'public','views'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ resave: true, saveUninitialized: true, secret: 'marisabernard' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico')); 
+app.set('superSecret', config.secret); // secret variable
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// ROUTES CONFIGURATION  ====================================================================
+require('./routes.js');
 
-var server = app.listen(80, function () {
+// SERVER INITIALIZATION =============================================================
+app.listen(80, function() {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
