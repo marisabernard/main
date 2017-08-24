88c0b0ec20f3bb85d140a3a1284e85aaefcaff18
Marisa <mlbernard91@gmail.com>
2017-08-24 09:40:52 -0400

    aws initial

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*eslint-env node */
-var express   	 	= require('express'); 
+/*var express   	 	= require('express'); 
 var bodyParser	 	= require('body-parser');
 var path 		 	= require('path');
 var logger 		 	= require('morgan');
@@ -34,3 +34,43 @@ app.listen(80, function() {
   var port = 80;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+*/
+var port = process.env.PORT || 3000,
+http = require('http'),
+fs = require('fs'),
+html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+if (req.method === 'POST') {
+    var body = '';
+
+    req.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function() {
+        if (req.url === '/') {
+            log('Received message: ' + body);
+        } else if (req.url = '/scheduled') {
+            log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+        }
+
+        res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+        res.end();
+    });
+} else {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+}
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
