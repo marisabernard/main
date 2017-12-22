15245c1bc6b394c938f8e7a713af2041ee2d0f05
Marisa Bernard <mlbernard91@gmail.com>
2017-12-22 17:02:39 -0500

    removed everything

--- app/show/show.entity.ts
+++
@@ -1,9 +0,0 @@
-export class Show {
-    public _id: string;
-    public title: string;
-  
-    constructor(id: string, title: string) {
-      this._id = id;
-      this.title = title;
-    }
-  }
