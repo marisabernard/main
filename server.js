61258434b98eb7050b4e95bd0da7662db13bd466
Marisa Bernard <mlbernard91@gmail.com>
2017-12-22 21:42:15 -0500

    committed

--- server.js
+++ server.js
@@ -1,42 +1,12 @@
 var port = process.env.PORT || 3000;
 var http = require('http');
 var fs = require('fs');
-var html = fs.readFileSync('index.html');
 var express = require('express');
 var path = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
 var api = require('./server/routes/api');
 var server = express();
-
-var log = function(entry) {
-  fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-/*var server = http.createServer(function (req, res) {
-if (req.method === 'POST') {
-    var body = '';
-
-    req.on('data', function(chunk) {
-        body += chunk;
-    });
-
-    req.on('end', function() {
-        if (req.url === '/') {
-            log('Received message: ' + body);
-        } else if (req.url = '/scheduled') {
-            log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-        }
-
-        res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-        res.end();
-    });
-} else {
-    res.writeHead(200);
-    res.write(html);
-    res.end();
-}
-});*/
 
 
 // Parsers for POST data
